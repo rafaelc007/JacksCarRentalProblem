80078846cc06a06ee860439fbe5d100736ac3196
rafcp <rafcp@motorola.com>
2020-05-06 10:45:56 -0400

    fixed remainder in car rental and written the first part of the policy improve script.

--- carRental.py
+++ carRental.py
@@ -4,19 +4,15 @@ class CarRental:
     num_of_cars = 0
     _rent_mean = None
     _return_mean = None
-    _rent_pmf = []
-    _return_pmf = []
     max_cars = 5
 
     def __init__(self, ren_m, ret_m, start_num=0):   # a lot can go wrong with this initialization
         if ren_m > 0:
             self._rent_mean = ren_m
-            self._rent_pmf = poisson_(self._rent_mean)
         else:
             raise Exception("Error setting rent mean parameter: must be > 0")
         if ret_m > 0:
             self._return_mean = ret_m
-            self._return_pmf = poisson_(self._return_mean)
         else:
             raise Exception("Error setting return mean parameter: must be > 0")
         if start_num >= 0:
@@ -45,5 +41,3 @@ class CarRental:
 
 if __name__ == "__main__":
     rent = CarRental(3,4)
-    print(rent._rent_pmf.f(11))
-    print(rent._return_pmf.f(11))
--- policy_improve.py
+++ policy_improve.py
@@ -1,4 +1,6 @@
 from scipy.stats import poisson
+import numpy as np
+import sys
 
 class poisson_:
 
@@ -45,3 +47,115 @@ class poisson_:
             Ret_value = 0
         finally:
             return Ret_value
+
+
+class location:
+    # A class holding the properties of a location together
+    def __init__(self, req, ret):
+        self.req_mean = req  # value of lambda for requests
+        self.ret_mean = ret  # value of lambda for returns
+        self.poisson_req = poisson_(self.req_mean)
+        self.poisson_ret = poisson_(self.ret_mean)
+
+
+# Location initialisation
+class ProblemDef:
+
+    @staticmethod
+    def max_cars():
+        return 20
+
+    @staticmethod
+    def disc_fact():
+        return 0.9
+
+    @staticmethod
+    def credit_reward():
+        return 10
+
+    @staticmethod
+    def moving_reward():
+        return -2
+    
+    A = location(3, 3)
+    B = location(4, 2)
+
+    value = np.zeros((max_cars()+1, max_cars()+1))
+    policy = value.copy().astype(int)
+
+
+    def expected_reward(self, state, action):
+        """
+        state  : It's a pair of integers, # of cars at A and at B
+        action : # of cars transferred from A to B,  -5 <= action <= 5 
+        """
+
+        rw = 0  # reward
+        new_state = [max(min(state[0] - action, self.max_cars()), 0), max(min(state[1] + action, self.max_cars()), 0)]
+
+        # adding reward for moving cars from one location to another (which is negative)
+
+        rw = rw + self.moving_reward() * abs(action)
+
+        # there are four discrete random variables which determine the probability distribution of the reward and next state
+
+        for Aa in range(self.A.poisson_req.alpha_param, self.A.poisson_req.beta_param):
+            for Ba in range(self.B.poisson_req.alpha_param, self.B.poisson_req.beta_param):
+                for Ab in range(self.A.poisson_ret.alpha_param, self.A.poisson_ret.beta_param):
+                    for Bb in range(self.B.poisson_ret.alpha_param, self.B.poisson_ret.beta_param):
+                        """
+                        Aa : sample of cars requested at location A
+                        Ab : sample of cars returned at location A
+                        Ba : sample of cars requested at location B
+                        Bb : sample of cars returned at location B
+                        Prob  : probability of this event happening
+                        """
+
+                        # all four variables are independent of each other
+                        Prob = self.A.poisson_req.vals[Aa] * self.B.poisson_req.vals[Ba] * \
+                               self.A.poisson_ret.vals[Ab] * self.B.poisson_ret.vals[Bb]
+
+                        valid_requests_A = min(new_state[0], Aa)
+                        valid_requests_B = min(new_state[1], Ba)
+
+                        rew = (valid_requests_A + valid_requests_B) * (self.credit_reward())
+
+                        # calculating the new state based on the values of the four random variables
+                        new_s = [0, 0]
+                        new_s[0] = max(min(new_state[0] - valid_requests_A + Ab, self.max_cars()), 0)
+                        new_s[1] = max(min(new_state[1] - valid_requests_B + Bb, self.max_cars()), 0)
+
+                        # Bellman's equation
+                        rw += Prob * (rew + self.disc_fact() * self.value[new_s[0]][new_s[1]])
+
+        return rw
+
+    # initial value of eps_param
+    _eps_param = 50
+
+    def policy_evaluation(self):
+
+        # here policy_evaluation has a static variable eps_param whose values decreases over time
+        eps_param = self._eps_param
+
+        self._eps_param /= 10
+
+        while True:
+            delta_param = 0
+
+            for i in range(self.value.shape[0]):
+                for j in range(self.value.shape[1]):
+                    # value[i][j] denotes the value of the state [i,j]
+
+                    old_val = self.value[i][j]
+                    self.value[i][j] = self.expected_reward([i, j], self.policy[i][j])
+
+                    delta_param = max(delta_param, abs(self.value[i][j] - old_val))
+
+                    print('.', end='')
+                    sys.stdout.flush()
+            print(delta_param)
+            sys.stdout.flush()
+
+            if delta_param < eps_param:
+                break
